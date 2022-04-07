ca8ebd4fc4b11e23756cd65b6a410efa4035bd26
soundmafi <soundmafi@gmail.com>
2022-04-07 23:27:14 +0300

    fix className links

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,18 +4,18 @@ import logo from '../../assets/app_logo.svg';
 const Nav = () => {
 	return (
 		<div className="nav">
-			<a href="" className="nav__logo">
+			<a href="#" className="nav__logo">
 				<img className="nav__picture" src={logo} alt="logo" />
 			</a>
 			<ul className="nav__list">
 				<li className="nav__item">
-					<a href="#">Features</a>
+					<a className='nav__link' href="#">Features</a>
 				</li>
 				<li className="nav__item">
-					<a href="#">Partners</a>
+					<a className='nav__link' href="#">Partners</a>
 				</li>
 				<li className="nav__item">
-					<a href="#">Stories</a>
+					<a className='nav__link' href="#">Stories</a>
 				</li>
 			</ul>
 			<button className="nav__btn" type='button'>Download for free</button>
